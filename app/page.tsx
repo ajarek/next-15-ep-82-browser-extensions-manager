6fe7a6e7664286486ba46bfd057ed4df010102a5
Ajarek <ajarek@poczta.onet.pl>
2025-03-23 00:06:04 +0100

    start template

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from '@/components/HeroSection'
+
+export default function Home() {
+  return (
+    <div className='min-h-[calc(100vh-64px)] flex flex-col justify-center items-center'>
+      <HeroSection />
+    </div>
+  )
+}
