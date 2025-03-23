422bc04b21087f2cd050bcf3747fcbb32b131da8
Ajarek <ajarek@poczta.onet.pl>
2025-03-24 00:57:41 +0100

    added system search

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/HeroSection'
 
-export default function Home() {
+export default async function Home({searchParams}: {  searchParams: Promise<{action: string}>}) {
+  const { action } = await searchParams
   return (
     <div className='min-h-[calc(100vh-64px)] flex flex-col justify-start items-center px-8 py-4'>
-      <HeroSection />
+      <HeroSection query={'action'} />
+      <div>{action}</div>
     </div>
   )
 }
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,13 +1,53 @@
+'use client'
 import { Button } from '@/components/ui/button'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
-const HeroSection = () => {
+interface SearchProps {
+  query: string
+}
+
+const HeroSection = ({ query }: SearchProps) => {
+  const searchParams = useSearchParams()
+  const { replace } = useRouter()
+  const pathname = usePathname()
+
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    if (term) {
+      params.set(`${query}`, term)
+    } else {
+      params.delete(`${query}`)
+    }
+    try {
+      replace(`${pathname}?${params.toString()}`)
+    } catch (error) {
+      console.error('Failed to replace URL parameters:', error)
+    }
+  }
+
   return (
     <div className='w-full flex  items-center justify-between text-center  px-4  '>
       <h1 className='text-2xl  font-semibold '>Extensions List</h1>
       <div className='flex items-center space-x-4'>
-        <Button className='rounded-xl bg-slate-500 hover:bg-primary focus:bg-primary transition-all delay-200 ease-in-out '>All</Button>
-        <Button className='rounded-xl bg-slate-500 hover:bg-primary focus:bg-primary transition-all delay-200 ease-in-out '>Active</Button>
-        <Button className='rounded-xl bg-slate-500 hover:bg-primary focus:bg-primary transition-all delay-200 ease-in-out '>Inactive</Button>
+        <Button
+          onClick={() => handleSearch('All')}
+          className='rounded-xl bg-slate-500 hover:bg-primary focus:bg-primary transition-all delay-200 ease-in-out '
+        >
+          All
+        </Button>
+        <Button
+          onClick={() => handleSearch('Active')}
+          className='rounded-xl bg-slate-500 hover:bg-primary focus:bg-primary transition-all delay-200 ease-in-out '
+        >
+          Active
+        </Button>
+        <Button
+          onClick={() => handleSearch('Inactive')}
+          className='rounded-xl bg-slate-500 hover:bg-primary focus:bg-primary transition-all delay-200 ease-in-out '
+        >
+          Inactive
+        </Button>
       </div>
     </div>
   )
